28ef3bd40c01c0f589faf21f82b1b12061a50e7b
Dengju Deng <wj871287@gmail.com>
2021-05-06 17:32:59 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,15 +2,7 @@ import { Duplex } from "stream";
 import WebSocket from 'ws';
 
 declare class WebSocketJSONStream extends Duplex {
-  private _emittedClose;
-  private ws;
   constructor(ws: WebSocket);
-  private _read(): void;
-  private _write(object: any, encoding: string, callback: (error?: Error | null) => void): void;
-  private _send(json: string, callback: (error?: Error | null) => void): void;
-  private _final(callback: (error?: Error | null) => void): void;
-  private _destroy(error: any, callback: (error: Error | null) => void): void;
-  private _closeWebSocket(code: number, reason?: string, callback?: (error?: Error | null) => void): void;
 }
 
 export default WebSocketJSONStream;
